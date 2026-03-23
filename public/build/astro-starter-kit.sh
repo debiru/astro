86e934cdc8f46f1f3fc3e51135a632ee78b27810
coeurl <coeurl@lavoscore.org>
2026-03-23 23:39:07 +0900

    astroStarterKit module; use import instead of fetch

--- public/build/astro-starter-kit.sh
+++ public/build/astro-starter-kit.sh
@@ -35,36 +35,36 @@ function git_commit_ifs() {
 # 1. Generate Astro Project
 [ ! -e "astro.config.mjs" ] && npm create astro@latest project -- --template minimal --no-install --no-git
 [ -d "project" ] && [ -n "$(ls -A project)" ] && mv project/* . && rm -r project
-git_commit_ifs ".vscode" "npm create astro@latest"
+git_commit_ifs ".vscode" "(ask-1) npm create astro@latest"
 
 # 2. Update .gitignore
 apply_patch "gitignore.patch"
-git_commit_if ".gitignore" "update .gitignore"
+git_commit_if ".gitignore" "(ask-2) update .gitignore"
 
 # 3. Generate deploy.yml
 apply_patch "deploy.yml.patch"
-git_commit_if ".github" "add .github/workflows/deploy.yml"
+git_commit_if ".github" "(ask-3) add .github/workflows/deploy.yml"
 
 # 4. Remove src/* files
 [ -d "src" ] && rm -r src
 [ ! -e "src" ] && mkdir src
-git_commit_ifs "src/" "remove default src files"
+git_commit_ifs "src/" "(ask-4) remove default src files"
 
 # 5. Update package.json
 apply_patch "package.json.patch"
 perl -i -pe "BEGIN{undef $/;} s@(?<=[^\n])\z@\n@smg" package.json
-git_commit_if "package.json" "update package.json"
+git_commit_if "package.json" "(ask-5) update package.json"
 
 # 6. Prepare starter kit
 apply_patch "starter-kit.patch"
 apply_patch "starter-kit-binary.patch"
 perl -i -pe "s@site: '[^']+'@site: 'https://${DOMAIN_NAME}'@smg" src/config/astro.mjs
-git_commit_ifs "public/favicon.ico" "generate starter kit"
+git_commit_ifs "public/favicon.ico" "(ask-6) generate starter kit"
 
 # 7. Generate package-lock.json
 npm install -D glob js-beautify sass stylelint stylelint-config-standard
-git_commit_ifs "package-lock.json" "npm install"
+git_commit_ifs "package-lock.json" "(ask-7) npm install"
 
 # 8. Build
 npm run build
-git_commit_ifs "astro.json" "npm run build"
+git_commit_ifs "astro.json" "(ask-8) npm run build"
